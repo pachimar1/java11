69a0e8743a798f0288b5df20c34e9f87bc8b49be
LeM <mihail.lebedev88@gmail.com>
2023-04-23 13:24:02 +0600

    add tests

--- src/test/java/ru/netology/javaqa/homework/TodosTest.java
+++ src/test/java/ru/netology/javaqa/homework/TodosTest.java
@@ -103,4 +103,52 @@ public class TodosTest {
         Task[] actual = todos.search("НетоБанк");
         Assertions.assertArrayEquals(expected, actual);
     }
+    @Test
+    public void shouldSearchNotMatch() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {};
+        Task[] actual = todos.search("Проблемы");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void shouldSearchThreeMatch() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям и попросить купить Молоко");
+
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                55,
+                "Выкатка 3й версии приложения",
+                "Приложение Молоко",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {simpleTask, epic, meeting};
+        Task[] actual = todos.search("Молоко");
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
